570787eafeb760d730b061b4cdf36089fce06bcd
Grega Bremec <gregab@p0f.net>
2019-08-05 18:05:22 -0400

    only recognise Tweet and Status now

--- camel/src/main/java/com/redhat/training/agile/components/StdoutBean.java
+++ camel/src/main/java/com/redhat/training/agile/components/StdoutBean.java
@@ -1,12 +1,16 @@
 package com.redhat.training.agile.components;
 
+import com.redhat.training.agile.model.Tweet;
+
 public class StdoutBean {
 	public void print(Object x) {
 		String message;
-		if (x instanceof twitter4j.Status) {
+		if (x instanceof Tweet) {
+			message = x.toString();
+		} else if (x instanceof twitter4j.Status) {
 			message = ((twitter4j.Status) x).getText();
 		} else {
-			message = x.toString();
+			message = "[StdoutBean] unsupported message format";
 		}
 		System.out.println(message);
 	}
