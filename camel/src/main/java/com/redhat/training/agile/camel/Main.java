4e132f5217ea00ef29c116e4a33a1e63d19851b9
Grega Bremec <gregab@p0f.net>
2019-08-05 18:04:58 -0400

    fixed settings to include env vars (2nd prio)

--- camel/src/main/java/com/redhat/training/agile/camel/Main.java
+++ camel/src/main/java/com/redhat/training/agile/camel/Main.java
@@ -9,23 +9,27 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		DefaultCamelContext c = new DefaultCamelContext();
 
-		// we need to do manual configuration here
 		TweetFollower tf = new TweetFollower();
 
-		if (System.getProperty("agile.camel.consumer.key", null) != null) {
-			tf.setConsumerKey(System.getProperty("agile.camel.consumer.key"));
+		// we need to do manual configuration here
+		tf.setConsumerKey(System.getProperty("agile.camel.consumer.key", System.getenv("TWITTER_CONSUMER_KEY")));
+		tf.setConsumerSecret(System.getProperty("agile.camel.consumer.secret", System.getenv("TWITTER_CONSUMER_SECRET")));
+		tf.setAccessToken(System.getProperty("agile.camel.access.token", System.getenv("TWITTER_ACCESS_TOKEN")));
+		tf.setAccessSecret(System.getProperty("agile.camel.access.secret", System.getenv("TWITTER_ACCESS_SECRET")));
+
+		String follow = System.getProperty("agile.camel.follow.account", System.getenv("TWITTER_ACCOUNT"));
+		if (follow != null && !"".equals(follow)) {
+			tf.setAccount(follow);
+		} else {
+			tf.setAccount("BBCSport");
 		}
-		if (System.getProperty("agile.camel.consumer.secret", null) != null) {
-			tf.setConsumerSecret(System.getProperty("agile.camel.consumer.secret"));
+
+		String poll = System.getProperty("agile.camel.follow.poll.interval", System.getenv("TWITTER_POLL_INTERVAL"));
+		if (poll != null && !"".equals(poll)) {
+			tf.setPollInterval(Integer.parseInt(poll));
+		} else {
+			tf.setPollInterval(30000);
 		}
-		if (System.getProperty("agile.camel.access.token", null) != null) {
-			tf.setAccessToken(System.getProperty("agile.camel.access.token"));
-		}
-		if (System.getProperty("agile.camel.access.secret", null) != null) {
-			tf.setAccessSecret(System.getProperty("agile.camel.access.secret"));
-		}
-		tf.setAccount(System.getProperty("agile.camel.follow.account", "BBCSport"));
-		tf.setPollInterval(Integer.parseInt(System.getProperty("agile.camel.follow.poll.interval", "30000")));
 
 		c.addRoutes(tf);
 		c.start();
