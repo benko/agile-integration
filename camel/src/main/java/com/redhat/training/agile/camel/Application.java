893abb36da678a549337e2af424c45c524b70023
Grega Bremec <gregab@p0f.net>
2019-08-05 12:48:24 -0400

    fixed autoscan package

--- camel/src/main/java/com/redhat/training/agile/camel/Application.java
+++ camel/src/main/java/com/redhat/training/agile/camel/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.redhat.training.agile.twitter")
+@ComponentScan(basePackages = "com.redhat.training.agile.routes")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
